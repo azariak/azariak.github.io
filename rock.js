2842f25e77bd77db8f4f5e14ee10531c3b9224c4
azariak <askelman5@gmail.com>
2016-08-30 17:52:03 -0400

    Update rock.js

--- rock.js
+++ rock.js
@@ -1,1 +1,15 @@
-object.onload=function(){myScript};
+object.onload=function(){var userchoice = prompt("Do you choose rock paper or scissors?");
+        var userchoice = userchoice.toLowerCase();
+        var computerchoice = Math.random();
+        if computerchoice = < 0.34 {
+            computerchoice = paper
+        } else if computerchoice = < 0.66 {
+            computerchoice = rock
+        } else computerchoice = > 0.66 {
+            computerchoice = scissors
+        }
+        var compare = function(userchoice, computerchoice) {
+    if(choice1 == choice2) {
+    return "The result is a tie!";
+}
+};
