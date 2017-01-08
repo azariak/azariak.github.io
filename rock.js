a5065936c796b861e2ffd22706edf6f6dc950dfe
azariak <askelman5@gmail.com>
2017-01-08 15:51:24 -0500

    Update rock.js

--- rock.js
+++ rock.js
@@ -1,15 +1,66 @@
-object.onload=function(){var userchoice = prompt("Do you choose rock paper or scissors?");
-        var userchoice = userchoice.toLowerCase();
-        var computerchoice = Math.random();
-        if computerchoice = < 0.34 {
-            computerchoice = paper
-        } else if computerchoice = < 0.66 {
-            computerchoice = rock
-        } else computerchoice = > 0.66 {
-            computerchoice = scissors
-        }
-        var compare = function(userchoice, computerchoice) {
-    if(choice1 == choice2) {
-    return "The result is a tie!";
+//for html play button do not delete
+function rock() {
+
+    var userchoice = prompt("Do you choose rock paper or scissors?").toLowerCase();
+    
+    //computer choosing rock paper or scissors
+    var computerchoice = Math.random();
+    if (computerchoice < .34)
+    {
+        computerchoice = "rock";
+    }
+    else if (computerchoice > .67)
+    {
+        computerchoice = "paper";
+    }
+    else {
+        computerchoice = "scissors";
+    }
+    //ends here
+    
+    //tie
+    if (computerchoice === userchoice)
+    {
+        alert ("Computer chose " + computerchoice+ ". It is a tie :|");
+    }
+    //tie ends
+    
+    
+    else if (computerchoice == "rock" && userchoice == "paper")
+    {
+        alert("Computer chooses rock. You win :)");
+    }
+    
+    else if (computerchoice == "rock" && userchoice == "scissors")
+    {
+        alert("Computer chooses rock. You lose :(");
+    }
+    
+    else if (computerchoice == "paper" && userchoice == "scissors")
+    {
+        alert("Computer chooses rock. You win :)");
+    }
+    
+    else if (computerchoice == "paper" && userchoice == "rock")
+    {
+        alert("Computer chooses paper. You lose :(");
+    }
+    
+    else if (computerchoice == "scissors" && userchoice == "rock")
+    {
+        alert("Computer chooses scissors. You win :)")
+    }
+    
+    else if(computerchoice == "scissors" && userchoice == "paper")
+    {
+        alert("Computer chooses scissors. You lose :(");
+    }
+
+    else {
+        alert("Error. Please only put in Rock, Paper or Scissors. Capitalization dosent matter.")
+    }
+    
+    
+    
+//do not erase it is part of function
 }
-};
