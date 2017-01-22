b006f6fee0319aa2cfeca4af8d606fec2e4db662
azariak <askelman5@gmail.com>
2017-01-22 16:26:38 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function rock() {
+var userchoice = prompt("Enter level code").toLowerCase();
+
+if userchoice == lvl1 {
+alert ("Press ok to go to level 1")
+window.location.href='https://azariak.github.io/lvl1';
+
+}
+
+
+}
