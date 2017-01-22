3d2448024268116a65c646438ac5a3cd77946905
azariak <askelman5@gmail.com>
2017-01-22 16:49:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function promo-code() {
   
-var userchoice = prompt("Enter level code").toLowerCase();
+var userchoice = window.prompt("Enter level code").toLowerCase();
 
 if userchoice == "lvl1" {
-alert ("Press ok to go to level 1")
-window.location.href='https://azariak.github.io/lvl1';
+(window.prompt('Do you wish to visit the following website?','https://azariak.github.io/lvl1'))
+  location.href='https://azariak.github.io/lvl1';
+ 
+
 }
 
 
