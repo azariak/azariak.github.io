9458bab1e9ddb9ebd1dbf86539b966167d8469cf
azariak <askelman5@gmail.com>
2017-04-07 17:44:06 -0400

    Create cointoss.js

---
+++ cointoss.js
@@ -0,0 +1,16 @@
+
+function cointoss() {
+
+    var toss =    var computerchoice = Math.random();
+
+    if (computerchoice > .50) {
+        alert("Heads");
+
+    }
+    
+        if (computerchoice < .50) {
+        alert("Tails");
+
+    }
+
+}     
